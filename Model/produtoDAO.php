be0e508d8493d69fd94a6bb1aa79e8ce51d7a221
natanlopes <natan1918@gmail.com>
2022-03-14 17:02:29 -0300

    Criando Modal
    UPDATE, getById,Delete

--- Model/produtoDAO.php
+++ Model/produtoDAO.php
@@ -15,14 +15,67 @@ $pstm -> bind_param("s", $value->getPreco());
 
 if ($pstm ->execute()) 
     return true;
-    # code...
+    
     else
     return false;
 
 }
 
+public function update(produtoVO $value){
+    $SQL ="UPDATE  produtos SET nome = ?, marca = ?, preco = ? WHERE id = ?";
+    
+    
+    $DB = new DB();
+    $DB -> getConnection();
+    $pstm = $DB-> execSQL($SQL);
+    
+    $pstm -> bind_param("s", $value->getNome());
+    $pstm -> bind_param("s", $value->getMarca());
+    $pstm -> bind_param("s", $value->getPreco());
+    $pstm -> bind_param("i", $value->getId());
+    if ($pstm ->execute()) 
+        return true;
+        
+        else
+        return false;
+    
+    }
 
+    public function update(produtoVO $value){
+        $SQL ="DELETE  FROM produtos WHERE Id=?";
+        
+        
+        $DB = new DB();
+        $DB -> getConnection();
+        $pstm = $DB-> execSQL($SQL);
+        
+        $pstm -> bind_param("i", $value->getId());
+        if ($pstm ->execute()) 
+            return true;
+            
+            else
+            return false;
+        
+        }
+        public function getById($id){
+            $SQL ="SELECT * FROM produtos WHERE id =".addslashes($id);
+            
+            
+            $DB = new DB();
+            $DB -> getConnection();
+            $query = $DB-> execReader($SQL);
+            
+        while ($reg = $query -> fetch_array(MYSSQLI_ASSOC)) {
+            $vo -> setId($reg["id"]);
+            $vo -> setNome($reg["nome"]);
+            $vo -> setMarca($reg["marca"]);
+            $vo -> setPreco($reg["preco"]);
+        }
+           return $vo; 
+            }
+            
 }
+
 
 
 
