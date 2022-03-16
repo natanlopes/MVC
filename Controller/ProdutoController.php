28f89fc8e15a1d4829239f7d05b37b16387e427c
natanlopes <natan1918@gmail.com>
2022-03-16 14:25:30 -0300

    Editar e retorno
     if($model->update ($vo)){
            $_SESSION["msg"] ="Produto atualizado  com sucesso.";
        }else {
            $_SESSION["msg"] ="ERRO ao Atualizar produto.";
        }

--- Controller/ProdutoController.php
+++ Controller/ProdutoController.php
@@ -12,15 +12,45 @@ $vo-> setMarca($_POST["txtMarca"]);
 $vo-> setPreco($_POST["txtPreco"]);
 
 if($model->insert ($vo)){
+    $_SESSION["msg"] ="Produto cadastro com sucesso.";
+}else {
+    $_SESSION["msg"] ="ERRO ao cadastrar produto.";
 }
+header ("Location: View/produtos/retorno.php");  
+}
+public function update(){
+    $model = new ProdutoModel();
+    $vo = new ProdutoVo();
+    $vo-> setId($_POST["txtId"]);
+    $vo-> setNome($_POST["txtNome"]);
+    $vo-> setMarca($_POST["txtMarca"]);
+    $vo-> setPreco($_POST["txtPreco"]);
     
-}
+    if($model->update ($vo)){
+        $_SESSION["msg"] ="Produto atualizado  com sucesso.";
+    }else {
+        $_SESSION["msg"] ="ERRO ao Atualizar produto.";
+    }
+    header ("Location: View/produtos/retorno.php");  
+    }
 public function novo(){
 
-    header ("Location: View/produtos/insert.php");
+    header ("Location: View/produtos/Insert.php");
 
 }
+public function editar(){
+    $model = new ProdutoModel();
 
+    $vo = $model-> getById($_GET["id"]);
+
+    $_SESSION["id"] = $vo-> getId();
+    $_SESSION["nome"] = $vo-> getNome();
+    $_SESSION["marca"] = $vo-> getMarca();
+    $_SESSION["preco"] = $vo-> getPreco();
+
+    header ("Location: View/produtos/edit.php");
+
+}
 }
 
 
