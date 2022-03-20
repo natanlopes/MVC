fbcab7baced50fc0a867647faf4facd69f1ae090
natanlopes <natan1918@gmail.com>
2022-03-20 11:15:29 -0300

    Create usuarioDAO.php
    class usuarioDAO implements IDAO{
        public function insert($value){}
          public function update($value){}
          public function delete($value){}
          public function getById($id){}
          public function getAll(){}

---
+++ Model/usuarioDAO.php
@@ -0,0 +1,11 @@
+<?php
+include("IDAO.php");
+
+class usuarioDAO implements IDAO{
+    public function insert($value){}
+      public function update($value){}
+      public function delete($value){}
+      public function getById($id){}
+      public function getAll(){}
+}
+?>
